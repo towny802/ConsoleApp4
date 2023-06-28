7249dcc3bde95bce9c2fe927218fddc3127139a6
towny802 <zbechhoefer@gmail.com>
2023-06-28 19:21:59 -0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+//using ExifLibNet;
+using ExifLibrary;
+
+namespace GetJPGMetadata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the path to the JPEG image.
+            string imagePath = @"C:\Users\admin\source\repos\ConsoleApp4\ConsoleApp4\angry_hoes.jpg";
+
+            // Create a new ImageFile object from the image file.
+            ImageFile file = ImageFile.FromFile(imagePath);
+
+
+
+            // Get the metadata from the image.
+            var metadata = file.Properties;
+
+            file.Properties.Set(ExifTag.WindowsComment, "yeah!!!", Encoding.ASCII);
+
+            // Print out the metadata.
+            foreach (var tag in metadata)
+            {
+                Console.WriteLine($"{tag.Name} = {tag.Value}");
+            }
+        }
+    }
+}
